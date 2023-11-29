e0ae746ea1f45700506aeb8a297532155f14a4ab
daniel8298 <daniel8298@gmail.com>
2023-11-29 15:44:59 +0200

    update test

--- src/dataAccess/mongoose.test.ts
+++ src/dataAccess/mongoose.test.ts
@@ -1,52 +1,58 @@
-import mongoose from "mongoose";
-import {
-  connectToMongoose,
-  getAllOrdersFromMongoDB,
-  getUserById,
-  insertOrderFromAPI,
-  insertOrdersIntoMongoose,
-} from "./mongoose";
+// import mongoose from "mongoose";
+// import {
+//   connectToMongoose,
+//   getAllOrdersFromMongoDB,
+//   getUserById,
+//   insertOrderFromAPI,
+//   insertOrdersIntoMongoose,
+// } from "./mongoose";
 
-describe("Test for MongoDB functions", () => {
-  //   beforeAll(async () => {
-  //     await connectToMongoose();
-  //   });
-
-  //   it("should connect to MongoDB successfully", () => {
-  //     expect(mongoose.connection).toBe(1);
-  //   });
-
-  // describe("Test for MongoDB functions", () => {
-  //   it("hello", async () => {
-  //     await connectToMongoose();
-  //     expect(process.env.MONGODB_URI).toBe(mongoose.connect());
-  //   });
-  // });
-
-  //   afterAll(async () => {
-  //     // Optionally disconnect from MongoDB after all tests
-  //     await mongoose.disconnect();
-  //   });
-
-  it("should insert orders into MongoDB", async () => {
-    await insertOrdersIntoMongoose();
-    const orders = await getAllOrdersFromMongoDB();
-    expect(orders.length).toBeGreaterThan(0);
-  });
-});
-
-//   it("should fetch user by ID", async () => {
-//     const user = await getUserById("your-user-id");
-//     expect(user).toBeDefined();
+// describe("Test for MongoDB functions", () => {
+//   beforeAll(async () => {
+//     await connectToMongoose();
 //   });
-
-//   it("should insert order from API", async () => {
-//     const order = {
-//       /* your order object */
-//     };
-//     const newOrder = await insertOrderFromAPI(order);
-//     expect(newOrder).toBeDefined();
+//   it("should insert orders into MongoDB", async () => {
+//     await insertOrdersIntoMongoose();
+//     const orders = await getAllOrdersFromMongoDB();
+//     expect(orders.length).toBeGreaterThan(0);
 //   });
-
-// Add more test cases for other functions...
 // });
+
+// //   it("should connect to MongoDB successfully", () => {
+// //     expect(mongoose.connection).toBe(1);
+// //   });
+
+// // describe("Test for MongoDB functions", () => {
+// //   it("hello", async () => {
+// //     await connectToMongoose();
+// //     expect(process.env.MONGODB_URI).toBe(mongoose.connect());
+// //   });
+// // });
+
+// //   afterAll(async () => {
+// //     // Optionally disconnect from MongoDB after all tests
+// //     await mongoose.disconnect();
+// //   });
+
+// //   it("should insert orders into MongoDB", async () => {
+// //     await insertOrdersIntoMongoose();
+// //     const orders = await getAllOrdersFromMongoDB();
+// //     expect(orders.length).toBeGreaterThan(0);
+// //   });
+// // });
+
+// //   it("should fetch user by ID", async () => {
+// //     const user = await getUserById("your-user-id");
+// //     expect(user).toBeDefined();
+// //   });
+
+// //   it("should insert order from API", async () => {
+// //     const order = {
+// //       /* your order object */
+// //     };
+// //     const newOrder = await insertOrderFromAPI(order);
+// //     expect(newOrder).toBeDefined();
+// //   });
+
+// // Add more test cases for other functions...
+// // });
